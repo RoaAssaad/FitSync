99b522c6eb3418946af8df169065e7ac1883d747
MichelMitri1 <michelmitri5@gmail.com>
2025-03-28 12:17:36 +0200

    added ui design to all the ui files

--- src/main/java/com/example/fitsync/Main.java
+++ src/main/java/com/example/fitsync/Main.java
@@ -3,7 +3,7 @@ package com.example.fitsync;
 import com.example.fitsync.ui.LoginScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
-`
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
